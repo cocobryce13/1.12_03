c3c8d70d1923b7aa3146d2a8b5ebc25595ddc8a7
sokol <sokol@ASUS>
2024-08-06 22:14:57 +0500

    Добавьте файлы проекта.

---
+++ 1.12_03.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<string>
+#include<fstream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::fstream fin("out.txt");
+    int number;
+    int size;
+    std::cout << "Введите размер массива: ";
+    std::cin >> size;
+    int* arr = new int[size];
+
+    if (!(fin.is_open()))
+    {
+        std::cout << "Not Open";
+        return 11111;
+    }
+
+    fin << size;
+    fin << "\n";
+
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "arr[" << i << "] = ";
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        fin << arr[i];
+    }
+
+    delete[] arr;
+    fin.close();
+
+    std::cout << "\n\n\n";
+
+    return 0;
+}
